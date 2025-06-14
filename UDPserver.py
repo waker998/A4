7456212500d565a540cd324a6ebfc9f3ace12ee1
waker998 <13909897901@163.com>
2025-06-14 19:32:35 +0800

    Update UDPserver.py
    Server monitoring port

--- UDPserver.py
+++ UDPserver.py
@@ -3,3 +3,10 @@ import threading
 import random
 import os
 import base64
+
+class UDPServer:
+    def __init__(self, port):
+        self.welcome_port = port
+        self.welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.welcome_socket.bind(('', self.welcome_port))
+        print(f"Server started on port {self.welcome_port}")
